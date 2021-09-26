0b1f65d06cc1941cafca98ce51760787dc034c3a
Chad Smith <smith.chad12@gmail.com>
2021-09-25 21:25:35 -0500

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Chad Smith on 9/25/21.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
