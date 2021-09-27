bfd51b6ccc957fcc81fb8c6a709021cda4bb9942
Chad Smith <smith.chad12@gmail.com>
2021-09-26 22:27:55 -0500

    add service to download coin data

--- CryptoApp/Presentation/Home/ViewModels/HomeViewModel.swift
+++ CryptoApp/Presentation/Home/ViewModels/HomeViewModel.swift
@@ -5,16 +5,25 @@
 //  Created by Chad Smith on 9/26/21.
 //
 
+import Combine
 import Foundation
 
 class HomeViewModel : ObservableObject {
     @Published var allCoins: [Coin] = []
     @Published var portfolioCoins: [Coin] = []
     
+    private let dataService = CoinDataService()
+    private var cancellabes = Set<AnyCancellable>()
+    
     init() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-            self.allCoins.append(DeveloperPreview.instance.coin)
-            self.portfolioCoins.append(DeveloperPreview.instance.coin)
-        }
+        addSubscribers()
+    }
+    
+    func addSubscribers() {
+        dataService.$allCoins
+            .sink { [weak self] returnedCoins in
+                self?.allCoins = returnedCoins
+            }
+            .store(in: &cancellabes)
     }
 }
